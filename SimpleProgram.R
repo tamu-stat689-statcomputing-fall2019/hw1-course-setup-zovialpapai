c3bf754d85413b980a37ec07210b7f65b2b3c7e0
Abhisek Chakraborty <cabhisek@stat.tamu.edu>
2019-08-31 02:06:11 -0500

    The RScript for Estimation of model parameters in the linear regression model is written

--- SimpleProgram.R
+++ SimpleProgram.R
@@ -9,7 +9,8 @@ beta = rep(2,p) # true vector of coefficients
 X = matrix(rnorm(n*p), n, p) # n by p matrix of predictors
 
 # [ToDo] Use generateY function to generate Y
-
+Y = generateY(X, beta, sigma, seed = 5832652) # Response Y is generated according to specified model parameters.
 # [ToDo] Use calculateBeta function to calculate beta_LS
-
+beta_LS = calculateBeta(X, Y) # The least square estimate of beta is calculated
 # [ToDo] Use calculateMSE to assess the estimation error measured by squared eucledian distance - ||beta - beta_LS||_2^2
+MSE = calculateMSE(beta, beta_LS)# MSE in estimation of beta is calculated.
